c18c2cf1c56bfa0d9956a4efcb770388d11eb291
Fauzi Fadhillah <fauzifadhi@gmail.com>
2021-11-19 17:35:35 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <TicTacToe msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import TicTacToe from './components/TicTacToe.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TicTacToe
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
